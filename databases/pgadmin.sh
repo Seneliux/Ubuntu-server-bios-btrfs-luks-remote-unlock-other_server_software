b08076f7f534b3bd31fbbf9f8adca57789e7b804
Seniux <47281748+Seneliux@users.noreply.github.com>
2021-02-07 09:39:42 +0100

    Update pgadmin.sh

--- databases/pgadmin.sh
+++ databases/pgadmin.sh
@@ -14,3 +14,26 @@ SQLITE_PATH = os.path.join(DATA_DIR, 'pgadmin4.db')
 SESSION_DB_PATH = os.path.join(DATA_DIR, 'sessions')
 STORAGE_DIR = os.path.join(DATA_DIR, 'storage')
 EOF
+python3 pgadmin4/lib/python3.8/site-packages/pgadmin4/setup.py
+pip install gunicorn
+deactivate
+chown -R www-data:www-data /var/www/pgadmin4
+touch /var/log/nginx/pgadmin4.log
+chown -R www-data:www-data /var/log/nginx/pgadmin4.log
+cat > /etc/systemd/system/pgadmin4.service << EOF
+[Unit]
+
+Description=pgAdmin4 service
+After=network.target
+
+[Service]
+User=www-data
+Group=www-data
+Environment="PATH=/var/www/pgadmin4/venv/bin"
+ExecStart=/var/www/pgadmin4/bin/gunicorn --bind unix:/tmp/pgadmin4.sock --workers=1 --threads=25 --chdir /var/www/pgadmin4/lib/python3.8/site-packages/pgadmin4 pgAdmin4:app
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl start pgadmin4
+systemctl enable pgadmin4
